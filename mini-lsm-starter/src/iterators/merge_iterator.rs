b0a9a947c26aac93d82d3a55061d3f17481ff673
Pinelliaw <wylbzc4928@gmail.com>
2024-02-05 00:55:32 +0800

    feat: day2 task2

--- mini-lsm-starter/src/iterators/merge_iterator.rs
+++ mini-lsm-starter/src/iterators/merge_iterator.rs
@@ -2,6 +2,7 @@
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
 use std::cmp::{self};
+use std::collections::binary_heap::PeekMut;
 use std::collections::BinaryHeap;
 
 use anyhow::Result;
@@ -47,7 +48,25 @@ pub struct MergeIterator<I: StorageIterator> {
 
 impl<I: StorageIterator> MergeIterator<I> {
     pub fn create(iters: Vec<Box<I>>) -> Self {
-        unimplemented!()
+        if iters.is_empty() {
+            return Self {
+                iters: BinaryHeap::new(),
+                current: None,
+            };
+        }
+
+        let mut heap = BinaryHeap::new();
+        for (idx, iter) in iters.into_iter().enumerate() {
+            if iter.is_valid() {
+                heap.push(HeapWrapper(idx, iter));
+            }
+        }
+
+        let current = heap.pop().unwrap();
+        Self {
+            iters: heap,
+            current: Some(current),
+        }
     }
 }
 
@@ -57,18 +76,76 @@ impl<I: 'static + for<'a> StorageIterator<KeyType<'a> = KeySlice<'a>>> StorageIt
     type KeyType<'a> = KeySlice<'a>;
 
     fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.current.as_ref().unwrap().1.key()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.current.as_ref().unwrap().1.value()
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.current.as_ref().map_or(false, |x| x.1.is_valid())
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        let current = self.current.as_mut().unwrap();
+        // peek_mut returns a mutable reference to the greatest item in the binary heap
+        // it means we will find the smallest key in the binary heap
+        while let Some(mut inner_iter) = self.iters.peek_mut() {
+            println!(
+                "inner_iter.1.key() {:?}",
+                String::from_utf8_lossy(inner_iter.1.key().raw_ref())
+            );
+            println!(
+                "inner_iter.value() {:?}",
+                String::from_utf8_lossy(inner_iter.1.value())
+            );
+            println!(
+                "current.1.key() {:?}",
+                String::from_utf8_lossy(current.1.key().raw_ref())
+            );
+            println!(
+                "current.1.value() {:?}",
+                String::from_utf8_lossy(current.1.value())
+            );
+            println!("current.0 {:?}", current.0);
+            //  We assume the one with a lower index (i.e., the first one) has the latest data.
+            if inner_iter.1.key() == current.1.key() {
+                if let e @ Err(_) = inner_iter.1.next() {
+                    PeekMut::pop(inner_iter);
+                    println!("e {:?}", e);
+                    return e;
+                }
+
+                if !inner_iter.1.is_valid() {
+                    PeekMut::pop(inner_iter);
+                }
+            } else {
+                break;
+            }
+        }
+        if let e @ Err(_) = current.1.next() {
+            return e;
+        }
+
+        if !current.1.is_valid() {
+            if let Some(iter) = self.iters.pop() {
+                *current = iter;
+            }
+            return Ok(());
+        }
+
+        // HeapWrapper ord rules:
+        // 1. compare the key of the iterator
+        // 2. if the key is the same, compare the index of the iterator
+        // 3. reverse the order of the comparison result
+        // so [0, 'a'] is bigger than [1, 'a'] and [0, 'e'] is smaller than [1, 'd']
+        if let Some(mut inner_iter) = self.iters.peek_mut() {
+            if *current < *inner_iter {
+                std::mem::swap(&mut *inner_iter, current);
+            }
+        }
+
+        Ok(())
     }
 }
